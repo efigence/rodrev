b95735ca834092ccb0328c7beba597150bffbcc8
Mariusz Gronczewski <xani666@gmail.com>
2021-02-11 11:08:40 +0100

    fence script

--- scripts/fence_rvd.pl
+++ scripts/fence_rvd.pl
@@ -10,108 +10,9 @@ use Getopt::Long qw(:config auto_help);
 use Pod::Usage;
 use Data::Dumper;
 use Sys::Syslog;
+use Sys::Syslog qw(:standard :macros);
 use Switch;
-
 openlog('fence_rvd', 'ndelay', 'daemon');
-syslog($priority, $format, @args);
-
-closelog();
-my $cfg = { # default config values go here
-    'rv-path' => '/usr/local/bin/rv',
-};
-my $help;
-
-
-# https://github.com/ClusterLabs/fence-agents/blob/master/doc/FenceAgentAPI.md
-
-# action - the operation (noted previously) to perform. This is one of the following (case insensitive): on, off, reboot, monitor, list, or status
-# ipaddr - for a hostname or IP address
-# login - for a username or login name
-# passwd - for a password
-# passwd_script - if your agent supports storing passwords outside of cluster.conf, this is a script used to retrieve your password (details on how this works will be added later). Generally, this script simply echoes the password to standard output (and is read in by the agent at run-time).
-# port - if you have to specify a plug or port (for example, on a network-enabled PDU with 8 ports)
-# nodename - if the agent fences by node name, this is the parameter to use (e.g. instead of port). In the event that both nodename and port are specified, the preference is given to port.
-
-
-my $parse_stdin=0;
-if ( !defined($ARGV[0])) {
-    $parse_stdin = 1;
-}
-
-while(<STDIN>) {
-    chomp;
-    my ($k, $v) = split(/=/, $_, 2);
-    $cfg->{$k} = $v;
-}
-
-GetOptions(
-    'action=s' => \$cfg->{'action'},
-    'ipaddr=s' => \$cfg->{'ipaddr'},
-    'login=s' => \$cfg->{'login'},
-    'passwd=s' => \$cfg->{'passwd'},
-    'passwd-script=s' => \$cfg->{'passwd_script'},
-    'port=s' => \$cfg->{'port'},
-    'nodename=s' => \$cfg->{'nodename'},
-    'help'          => \$help,
-) or pod2usage(
-    -verbose => 2,  #2 is "full man page" 1 is usage + options ,0/undef is only usage
-    -exitval => 1,   #exit with error code if there is something wrong with arguments so anything depending on exit code fails too
-);
-
-
-
-
-
-# some options are required, display short help if user misses them
-my $required_opts = [ 'nodename','action' ];
-my $missing_opts;
-foreach (@$required_opts) {
-    if (!defined( $cfg->{$_} ) ) {
-        push @$missing_opts, $_
-    }
-}
-
-if ($help || defined( $missing_opts ) ) {
-    my $msg;
-    my $verbose = 2;
-    if (!$help && defined( $missing_opts ) ) {
-        $msg = 'Opts ' . join(', ',@$missing_opts) . " are required!\n";
-        $verbose = 1; # only short help on bad arguments
-    }
-    pod2usage(
-        -message => $msg,
-        -verbose => $verbose, #exit code doesnt work with verbose > 2, it changes to 1
-    );
-}
-syslog('info', "running [$cfg->{'action'}] on [$cfg->{'nodename'}]");
-syslog('info', Dumper $config);
-
-
-switch(lc($cfg->{'action'})) {
-    case /status|monitor/ {
-        system($cfg->{'rv-path'}, 'fence', 'status', $cfg->{'nodename'});
-        my $exit_code = $? >> 8;
-        exit $exit_code;
-    }
-    case /off|reboot/ {
-        system($cfg->{'rv-path'}, 'fence', 'run', $cfg->{'nodename'});
-        my $exit_code = $? >> 8;
-        exit $exit_code;
-    }
-    case /on/ {
-        # TODO no idea how to verify restart yet
-        exit 0;
-    }
-    case /metadata/ {
-        metadata();
-        exit 0;
-    }
-    else {
-        print "command [$cfg->{'action'}] not supported"
-    }
-}
-
-closelog();
 
 
 sub metadata() {
@@ -142,6 +43,104 @@ fence_rvd sends fencing request to rodrev via queue
 </resource-agent>
 ';
 }
+
+my $cfg = { # default config values go here
+    'rv-path' => '/usr/local/bin/rv',
+};
+my $help;
+
+
+# https://github.com/ClusterLabs/fence-agents/blob/master/doc/FenceAgentAPI.md
+
+# action - the operation (noted previously) to perform. This is one of the following (case insensitive): on, off, reboot, monitor, list, or status
+# ipaddr - for a hostname or IP address
+# login - for a username or login name
+# passwd - for a password
+# passwd_script - if your agent supports storing passwords outside of cluster.conf, this is a script used to retrieve your password (details on how this works will be added later). Generally, this script simply echoes the password to standard output (and is read in by the agent at run-time).
+# port - if you have to specify a plug or port (for example, on a network-enabled PDU with 8 ports)
+# nodename - if the agent fences by node name, this is the parameter to use (e.g. instead of port). In the event that both nodename and port are specified, the preference is given to port.
+
+
+my $parse_stdin=0;
+if ( !defined($ARGV[0])) {
+    $parse_stdin = 1;
+}
+
+while(<STDIN>) {
+    chomp;
+    my ($k, $v) = split(/=/, $_, 2);
+    $cfg->{$k} = $v;
+}
+
+GetOptions(
+    'o|action=s' => \$cfg->{'action'},
+    'ipaddr=s' => \$cfg->{'ipaddr'},
+    'login=s' => \$cfg->{'login'},
+    'passwd=s' => \$cfg->{'passwd'},
+    'passwd-script=s' => \$cfg->{'passwd_script'},
+    'port=s' => \$cfg->{'port'},
+    'nodename=s' => \$cfg->{'nodename'},
+    'help'          => \$help,
+) or pod2usage(
+    -verbose => 2,  #2 is "full man page" 1 is usage + options ,0/undef is only usage
+    -exitval => 1,   #exit with error code if there is something wrong with arguments so anything depending on exit code fails too
+);
+
+syslog('info', Dumper $cfg);
+
+if ($cfg->{'action'} eq 'metadata') {
+    &metadata();
+    exit 0;
+}
+
+# some options are required, display short help if user misses them
+my $required_opts = [ 'nodename','action' ];
+my $missing_opts;
+foreach (@$required_opts) {
+    if (!defined( $cfg->{$_} ) ) {
+        push @$missing_opts, $_
+    }
+}
+
+if ($help || defined( $missing_opts ) ) {
+    my $msg;
+    my $verbose = 2;
+    if (!$help && defined( $missing_opts ) ) {
+        $msg = 'Opts ' . join(', ',@$missing_opts) . " are required!\n";
+        $verbose = 1; # only short help on bad arguments
+    }
+    pod2usage(
+        -message => $msg,
+        -verbose => $verbose, #exit code doesnt work with verbose > 2, it changes to 1
+    );
+}
+syslog('info', "running [$cfg->{'action'}] on [$cfg->{'nodename'}]");
+
+
+switch(lc($cfg->{'action'})) {
+    case /status|monitor/ {
+        system($cfg->{'rv-path'}, 'fence', 'status', $cfg->{'nodename'});
+        my $exit_code = $? >> 8;
+        exit $exit_code;
+    }
+    case /off|reboot/ {
+        system($cfg->{'rv-path'}, 'fence', 'run', $cfg->{'nodename'});
+        my $exit_code = $? >> 8;
+        exit $exit_code;
+    }
+    case /on/ {
+        # TODO no idea how to verify restart yet
+        exit 0;
+    }
+    else {
+        print "command [$cfg->{'action'}] not supported"
+    }
+}
+
+closelog();
+
+
+
 
 
 __END__
